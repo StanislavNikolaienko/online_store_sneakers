2624cae50f41b365f1506fc2c7d78f10978994f0
stanislav <lettersforstanislav@gmail.com>
2023-05-04 18:23:13 +0300

    add how to start the project with README.md, remove commited code

--- store/config/settings.py
+++ store/config/settings.py
@@ -58,18 +58,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": config("DB_NAME"),
-#         "USER": config("DB_USER"),
-#         "PASSWORD": config("DB_PASSWORD"),
-#         "HOST": config("DB_HOST"),
-#         "PORT": config("DB_PORT"),
-#     }
-# }
-
-# for docker
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -80,6 +68,8 @@ DATABASES = {
         "PORT": "5432",
     }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
