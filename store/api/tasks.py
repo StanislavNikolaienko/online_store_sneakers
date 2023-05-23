4909c3cb79ecb90847d344da707b39a8396db76a
stanislav <lettersforstanislav@gmail.com>
2023-05-23 18:30:57 +0300

    add parameters

--- store/api/tasks.py
+++ store/api/tasks.py
@@ -1,7 +1,7 @@
-from apps.shop_app.sneakers_db_update import update_db
+from apps.shop_app.rapidapi.sneakers_db_update import update_db
 from celery import shared_task
 
 
 @shared_task
-def update_sneakers_db():
-    update_db()
+def update_sneakers_db(data):
+    update_db(data)
